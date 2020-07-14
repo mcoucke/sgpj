1b70525abea9ee8f005abf4f4fa4f8eda0e4d073
Maxime Coucke <maxime.coucke@etu.univ-orleans.fr>
2020-07-14 15:28:29 +0200

    adding serv

---
+++ serv/src/main/java/com/mcoucke/sgpj/SgpjApplication.java
@@ -0,0 +1,13 @@
+package com.mcoucke.sgpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgpjApplication.class, args);
+    }
+
+}
---
+++ serv/src/test/java/com/mcoucke/sgpj/SgpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcoucke.sgpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
